6704ac07c7f466609c8ce8ab4d74fe2e0fc1c950
Bryan Runck <runck014@umn.edu>
2019-10-25 13:12:43 +0000

    added scriptwq VNCServer run at startup

--- gateway/gateway_setup.sh
+++ gateway/gateway_setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 sudo apt update
-sudo apt install realvnc-vnc-server real-vnc-vnc-viewer
+#sudo apt install realvnc-vnc-server real-vnc-vnc-viewer
 sudo apt-get install tightvncserver
-
 sudo apt-get install vim
+
+
+## Create and edit autostart options for Rpi
+## Will make VNC Server run at startup
+## so RPi is still accessible from VNCViewer with reboot
+cd /home/pi
+cd .config
+mkdir autostart
+cd autostart
+echo -e "[Desktop Entry]\nType=Application\nName=TightVNC\nExec=vncserver :1\nStartupNotify=false"
+
+
+
 
 vncserver:1
