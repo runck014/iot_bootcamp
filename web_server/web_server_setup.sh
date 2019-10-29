b8bc32e9ed57ece527bd07d1a139e4035573da6f
Bryan Runck <runck@Bryans-MacBook-Pro.local>
2019-10-28 21:06:59 -0500

    included download of pymongo

--- web_server/web_server_setup.sh
+++ web_server/web_server_setup.sh
@@ -5,12 +5,11 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 rm get-pip.py # clean up
 
-# 2. install python packages
+# 2. install #python packages
 sudo pip install flask
-#mongodb interface
+sudo pip install pymongo
 
 #3. start flask app
-export FLASK_APP= #name of app
-flask run --host=0.0.0.0
+#python NAME_OF_APP.py
 
 
