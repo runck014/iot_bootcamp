82177578d1880d113462b43d1276a25ea7e369c7
Bryan Runck <runck014@umn.edu>
2019-10-29 13:57:09 +0000

    added notes on setting up UNSECURE password only ssh access

--- web_server/web_server_setup.sh
+++ web_server/web_server_setup.sh
@@ -10,8 +10,17 @@ sudo pip install flask
 sudo pip install pymongo
 sudo python -m pip install pymongo[srv]
 
+# 3. Allow password only access to web server.
+# NO NOT DO THIS FOR PRODUCTION. ONLY FOR BOOTCAMP.
+# VERY UNSECURE.
 
-#3. start flask app
+# sudo adduser <name>
+# sudo usermod -aG sudo <name>
+# edit file: /etc/ssh/ssh_config
+# PasswordAuthentication = Yes
+# sudo service ssh restart
+
+# 4. start flask app
 #python NAME_OF_APP.py
 
 
