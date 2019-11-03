6c83048d14cdbbfc6a0322f765e905b78469a0e7
Bryan Runck <runck@Bryans-MacBook-Pro.local>
2019-11-03 18:03:07 +0100

    Chantigian added node comments

--- node/node_lora32u4.ino
+++ node/node_lora32u4.ino
@@ -3,18 +3,18 @@
 // 2019
  
 // Contributors: Bryan Chantigian, Thor Solberg, Pete Marchetto, Bryan Runck
+#include <SPI.h>    //This library allows for SPI communications to be used for the radio module as well as any other sensors that use SPI
+#include <LoRa.h>   //The LoRa library allows for the LoRa communications to function. It imports all the initalization functions as well as the packet sending and recieving abilities
+#include <MD_TCS230.h>  //This library allows for the LoRa32u4 to communicate with the PAR sensor
+//#include <FreqCount.h>  //I don't believe this library is used, most likely can be commended out
+#include <Wire.h>   //This library allows for I2C communications on the LoRa board
+#include <OneWire.h>  //This library allows for the LoRa to communicate with the DS18B20 temp sensor using onewire protocol
+#include <DallasTemperature.h>  //This libaray also is required to communicate with the DS18B20 for the temperature measurments
+#include <cactus_io_BME280_I2C.h>   //Allows for the BME280 to work with the LoRa32u4 and to pull data from the BME280
+#include <EEPROM.h> //Allows for interaction with the EEPROM of the LoRa32u4, mainly for the UID assigning
 
-#include <SPI.h>
-#include <LoRa.h>
-#include <MD_TCS230.h>
-#include <FreqCount.h>
-#include <Wire.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <cactus_io_BME280_I2C.h>
-#include <EEPROM.h>
-
-//LoR32u4 915MHz V1.2 (white board)
+//SPI setup for the radio module on LoRa32u4
+//the numbers are set pins on the radio module to allow for communication between the LoRa32u4 microcontroller and radio module
 #define SCK     15
 #define MISO    14
 #define MOSI    16
@@ -26,16 +26,16 @@
 #define BAND 915E6    //for 915MHz
 #define PABOOST true
 
-//sets onewire comm pin
+//sets onewire comm pin. This may change depending on physical build
 #define oneWire_Bus A0
 
-//sets moisure sensor pin
+//sets moisure sensor pin. This may change depending on physical build
 #define soilMoisture A1
 
-//sets CAPACITIVE soil moisture sensor
+//sets CAPACITIVE soil moisture sensor. This may change depending on physical build
 #define soilMoistCAP A2
 
-//sets color sensor pins
+//sets color sensor pins. These may change depending on physical build
 #define S2 13
 #define S3 12
 #define OE 11
@@ -46,7 +46,7 @@ DallasTemperature tempSensor(&onewire); //configures temp sensor to onewire bus
 
 int uid;
 
-//This function sets the PAR sensor to sense different colors by changing the selection pins
+//This function sets the PAR sensor to sense different colors by changing the selection pins It returns the the time of the low signal in microseconds
 long senseColor(char color)
 {
   if (color == 'W')
@@ -88,7 +88,7 @@ void setup()
   //sets the pins for LoRa comms
   Serial.begin(9600);
   LoRa.setPins(SS, RST, DI0);
-  LoRa.begin((long)BAND, PABOOST);  //This line may need to have PABOOST deleted before uploading, some weird things happen sometimes
+  LoRa.begin((long)BAND);  //This line may need to have PABOOST deleted before uploading, some weird things happen sometimes
 
   // Starts bme and temp sensor
   bme.begin();
@@ -97,7 +97,7 @@ void setup()
   //sets cap soil moist to input
   pinMode(soilMoistCAP, INPUT);
 
-  //setup Color Sensor
+  //setup Color Sensor pins
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(OE, INPUT);
@@ -105,19 +105,20 @@ void setup()
 
 void loop()
 {
-  bme.readSensor();   //required to get separate readings
+  bme.readSensor();   //required to get separate readings, this sends a request for data retrieval
   tempSensor.requestTemperatures();   //sends bit-stream to request temp readings
 
   //CAP SOIL moist
   int SMCAP = analogRead(soilMoistCAP);
 
   //get Color data
-  int red = senseColor('R');    //outputs time of low signal read in microseconds
+  int red = senseColor('R');    //outputs time of low signal read in microseconds using the pulseIn function
   int blue = senseColor('B');
   int green = senseColor('G');
   int white = senseColor('W');
 
   //LoRa comms
+  //These lines of code will construct and send the packet via LoRa protocol
   LoRa.beginPacket();
   LoRa.print("{ \"sensor\":\"WASECA\",");
 
@@ -166,6 +167,8 @@ void loop()
   LoRa.print("\" }");
   LoRa.endPacket();
 
+
+  //Serial port writing for debugging
   Serial.print("TempBME: ");
   Serial.print(bme.getTemperature_C());
   Serial.print("\t");
@@ -199,3 +202,4 @@ void loop()
 
   delay(5000);
 }
+
