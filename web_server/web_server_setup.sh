1942d3f817a98d1460bb72cb573387c8846e52c2
Bryan Runck <runck014@umn.edu>
2022-02-10 11:04:40 -0600

    Update web_server_setup.sh

--- web_server/web_server_setup.sh
+++ web_server/web_server_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+  
 # 0. Get Python
-sudo apt-get install python -y
+sudo apt-get install python
 
 # 1. Get pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 sudo python get-pip.py
 rm get-pip.py # clean up
 
@@ -16,14 +16,10 @@ sudo python -m pip install pymongo[srv]
 # 3. Allow password only access to web server.
 # NO NOT DO THIS FOR PRODUCTION. ONLY FOR BOOTCAMP.
 # VERY UNSECURE.
-
 # sudo adduser <name>
 # sudo usermod -aG sudo <name>
 # edit file: /etc/ssh/ssh_config
 # PasswordAuthentication = Yes
 # sudo service ssh restart
-
 # 4. start flask app
 #python NAME_OF_APP.py
-
-
